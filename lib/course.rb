69ffaae25bf75e887151c56209f64b8e8c416791
Hoon Lee <yl.hoony@gmail.com>
2017-12-14 18:26:56 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,6 @@
-
+class Course
+  
+end
 
 
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
- 
+
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +15,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
