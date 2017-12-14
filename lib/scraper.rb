df261bfa8fe68c0973436800589b5a86acd2ecd7
Hoon Lee <yl.hoony@gmail.com>
2017-12-14 19:08:21 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,36 +7,34 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
-    binding.pry
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
-  # def get_courses
-  #   doc = self.get_page
-  #   doc.css(".posts-holder article")
-  # end
-  #
-  # def make_courses
-  #   courses = self.get_courses
-  #   courses.each do |course|
-  #     e = Course.new
-  #     e.title = course.css("h2").text
-  #     e.schedule = course.css(".date").text
-  #     e.description = course.css("p").text
-  #   end
-  # end
-  #
-  # def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
+  def get_courses
+    doc = self.get_page
+    doc.css(".post")
+  end
+  
+  def make_courses
+    courses = self.get_courses
+    courses.each do |course|
+      e = Course.new
+      e.title = course.first.css("h2").text
+      e.schedule = course.first.css(".date").text
+      e.description = course.first.css("p").text
+    end
+  end
+  
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
 
 end
 
